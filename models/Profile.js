f7ff2eed9028443f09db045c41ac877597c18a39
lwandle <ndulilm@gmail.com>
2018-07-01 19:50:53 +0200

    Done with profile display

--- models/Profile.js
+++ models/Profile.js
@@ -4,146 +4,117 @@ const Schema = mongoose.Schema;
 // Create Schema
 const ProfileSchema = new Schema({
   user: {
-      type: Schema.Types.ObjectId,
-      ref: 'users'
+    type: Schema.Types.ObjectId,
+    ref: "users"
   },
   handle: {
-      type: String,
-      required: true,
-      max: 40
+    type: String,
+    required: true,
+    max: 40
   },
   company: {
-      type: String
+    type: String
   },
-  website:{
-      type: String
+  website: {
+    type: String
   },
   location: {
-      type: String
+    type: String
   },
   status: {
-      type: String,
-      required: true
+    type: String,
+    required: true
   },
   skills: {
-      type: [String],
-      required: true
+    type: [String],
+    required: true
   },
-  bio:{
-      type: String
+  bio: {
+    type: String
   },
   githubusername: {
-      type: String
+    type: String
   },
-  experience:[
-      {
-          title:{
-             type: String,
-             required: true
-          },
-          company:{
-              type: String,
-              required: true
-          },
-          location: {
-              type: String
-          },
-          from: {
-              type: Date,
-              required: true
-          },
-          to: {
-              type: Date
-          },
-          current: {
-              type: Boolean,
-              default: false
-          },
-          discription:{
-              type: String
-          }
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
       }
-  ],
-    experience:[
-      {
-          title:{
-             type: String,
-             required: true
-          },
-          company:{
-              type: String,
-              required: true
-          },
-          location: {
-              type: String
-          },
-          from: {
-              type: Date,
-              required: true
-          },
-          to: {
-              type: Date
-          },
-          current: {
-              type: Boolean,
-              default: false
-          },
-          discription:{
-              type: String
-          }
-      }
+    }
   ],
   education: [
-      {
-            school: {
-               type: String,
-               required: true
-            },
-            degree: {
-                type: String,
-                required: true
-            },
-            fieldofstudy: {
-                type: String,
-                required: true
-            },
-            from: {
-                type: Date,
-                required: true
-            },
-            to: {
-                type: Date
-            },
-            current: {
-                type: Boolean,
-                default: false
-            },
-            discription: {
-                type: String
-            }
-        }
-    ],
-    social:{
-        youtube:{
-            type:String
-        },
-        twitter:{
-            type: String
-        },
-        facebook: {
-            type: String
-        },
-        linkedin: {
-            type: String
-        },
-        instagram: {
-            type: String
-        }
-    },
-    date:{
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
         type: Date,
-        default: Date.now
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
     }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = Profile = mongoose.model('profile', ProfileSchema);
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
